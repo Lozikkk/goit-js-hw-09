45fc0c0dcdb365a8ca921d4a7bbb42b16dba7832
Oleksandr <lozikanet@gmail.com>
2021-11-16 12:02:01 +0200

    timer

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  delay: document.querySelector('input[name="delay"]'),
+  step: document.querySelector('input[name="step"]'),
+  amount: document.querySelector('input[name="amount"]'),
+  button: document.querySelector('button[type="submit"]'),
+};
+refs.button.addEventListener('click', onBtnClick);
+
+function onBtnClick(event) {
+  event.preventDefault();
+
+  const { delay, step, amount } = refs;
+  if (!delay.value || !step.value || !amount.value) {
+    Notify.failure(`Error: all fields must be filled in !`);
+    return;
   }
+  const amountEl = +amount.value;
+  const stepEl = +step.value;
+  let delayEl = +delay.value;
+
+  for (let i = 0; i < amountEl; i += 1) {
+    const position = i + 1;
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+    delayEl += stepEl;
+  }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
